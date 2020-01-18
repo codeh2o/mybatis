dd812253dbd2175e3f013c2c7b39afab83a2d611
h2o <h2o@example.com>
2020-01-18 23:34:04 +0800

    first commit

---
+++ src/main/java/com/imooc/myBatis/utils/MyBatisUtils.java
@@ -0,0 +1,37 @@
+package com.imooc.myBatis.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MyBatisUtils {
+    private static SqlSessionFactory sqlSessionFactory = null;
+
+    static {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = null;
+        try {
+            inputStream = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new ExceptionInInitializerError();
+        }
+
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+    }
+
+    public static SqlSession openSession() {
+        return sqlSessionFactory.openSession();
+    }
+
+    public static void closeSession(SqlSession sqlSession) {
+        if (sqlSession != null) {
+            sqlSession.close();
+        }
+    }
+}
---
+++ src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -0,0 +1,51 @@
+package com.imooc.mybatis;
+
+import com.imooc.myBatis.utils.MyBatisUtils;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+
+public class MyBatisTestor {
+    @Test
+    public void testSqlSessionFactory() throws IOException {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = sqlSessionFactory.openSession();
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+
+            }
+
+
+        }
+    }
+
+    @Test
+    public void testMyBatisUtils() throws Exception {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Connection connection = sqlSession.getConnection();
+            System.out.println(connection);
+        } catch (Exception e) {
+            throw e;
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+}
